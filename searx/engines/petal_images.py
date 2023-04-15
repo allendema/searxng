8d08df3acbe9f4fcd55d19d21e84676b19ed4fcb
allendema_searxng_pi <pi@raspberrypi.local>
2023-04-15 11:57:50 +0200

    [fix] fix petalseach images ua

--- searx/engines/petal_images.py
+++ searx/engines/petal_images.py
@@ -12,35 +12,39 @@ from lxml import html
 from searx.utils import extract_text
 
 about = {
-    "website": 'https://petalsearch.com/',
-    "wikidata_id": 'Q104399280',
+    "website": "https://petalsearch.com/",
+    "wikidata_id": "Q104399280",
     "official_api_documentation": False,
     "use_official_api": False,
     "require_api_key": False,
-    "results": 'JSON',
+    "results": "JSON",
 }
 
-categories = ['images']
+categories = ["images"]
 paging = True
-time_range_support = False
 
 safesearch = True
-safesearch_table = {0: 'off', 1: 'moderate', 2: 'on'}
+safesearch_table = {0: "off", 1: "moderate", 2: "on"}
 
-base_url = 'https://petalsearch.com/'
-search_string = 'search?{query}&channel=image&ps=50&pn={page}&region={lang}&ss_mode={safesearch}&ss_type=normal'
+
+base_url = "https://petalsearch.com/"
+search_string = "search?{query}&channel=image&ps=50&pn={page}&region={lang}&ss_mode={safesearch}&ss_type=normal"
 
 
 def request(query, params):
 
     search_path = search_string.format(
-        query=urlencode({'query': query}),
-        page=params['pageno'],
-        lang=params['language'].lower(),
-        safesearch=safesearch_table[params['safesearch']],
+        query=urlencode({"query": query}),
+        page=params["pageno"],
+        lang=params["language"].lower(),
+        safesearch=safesearch_table[params["safesearch"]],
     )
 
-    params['url'] = base_url + search_path
+    params["url"] = base_url + search_path
+    params["headers"] = {
+        "User-Agent": ("Mozilla/5.0 (Linux; Android 7.0;) AppleWebKit/537.36 (KHTML, like Gecko)"
+                       "Mobile Safari/537.36 (compatible; PetalBot;+https://webmaster.petalsearch.com/site/petalbot)")
+    }
 
     return params
 
@@ -49,46 +53,45 @@ def response(resp):
     results = []
 
     tree = html.fromstring(resp.text)
-    root = tree.findall('.//script[3]')
 
-    # Convert list to JSON
-    json_content = extract_text(root)
+    found_json = tree.find('.//*[@type="application/json"]')
+    json_content = extract_text(found_json)
 
-    # Manipulate with JSON
     data = loads(json_content)
 
-    for result in data['newImages']:
-        url = result['url']
-        title = result['title']
-        thumbnail_src = result['image']
+    for result in data["imagePreviewData"]["data"]:
+        url = result["link"]
+        title = result["title"]
+        thumbnail_src = result["image"]
+        date_from_api = result.get("publishTime")
 
-        pic_dict = result.get('extrainfo')
+        pic_dict = result.get("itemContent")
 
-        date_from_api = pic_dict.get('publish_time')
-        width = pic_dict.get('width')
-        height = pic_dict.get('height')
-        img_src = pic_dict.get('real_url')
+        width = pic_dict.get("width")
+        height = pic_dict.get("height")
+        img_src = pic_dict.get("real_url")
 
-        # Continue if img_src is missing
-        if img_src is None or '':
+        if img_src is None:
             continue
 
-        # Get and convert published date
-        if date_from_api is not None:
+        if date_from_api != "0":
             publishedDate = datetime.fromtimestamp(int(date_from_api))
 
-        # Append results
         results.append(
             {
-                'template': 'images.html',
-                'url': url,
-                'title': title,
-                'img_src': img_src,
-                'thumbnail_src': thumbnail_src,
-                'width': width,
-                'height': height,
-                'publishedDate': publishedDate,
+                "template": "images.html",
+                "url": url,
+                "title": title,
+                "img_src": img_src,
+                "thumbnail_src": thumbnail_src,
+                "width": width,
+                "height": height,
+                "publishedDate": publishedDate,
+                "upstream_extra": pic_dict,
             }
         )
+
+    for suggestion in data["relatedSearchList"]:
+        results.append({"suggestion": suggestion})
 
     return results
